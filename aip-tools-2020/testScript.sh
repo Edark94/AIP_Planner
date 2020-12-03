49844412637b24888c9cb336a71a316f7cda8aa5
Edark94 <edark_94@hotmail.com>
2020-12-03 19:33:02 +0000

    Added data

--- aip-tools-2020/testScript.sh
+++ aip-tools-2020/testScript.sh
@@ -29,8 +29,9 @@
 
 time='1800s'
 path='./src/floor-tile-sequential-optimal/'
-planner='merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false),merge_strategy=merge_sccs(order_of_sccs=topological,merge_selector=score_based_filtering(scoring_functions=[goal_relevance,dfp,total_order])),label_reduction=exact(before_shrinking=true,before_merging=false),max_states=50k,threshold_before_merge=1)'
-folder='merge_and_shrink'
+planner='hmax()'
+#planner='merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false),merge_strategy=merge_sccs(order_of_sccs=topological,merge_selector=score_based_filtering(scoring_functions=[goal_relevance,dfp,total_order])),label_reduction=exact(before_shrinking=true,before_merging=false),max_states=50k,threshold_before_merge=1)'
+folder='hmax'
 
 # ---------------------------------- 
 domain="${path}domain.pddl"
